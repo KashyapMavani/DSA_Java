e56b562e06e60ec7d5d969bef84370066a940c9c
KashyapMavani <kmavani1807@gmail.com>
2024-12-29 13:18:50 +0530

    ...

--- CountDigit.java
+++ CountDigit.java
@@ -1,14 +1,21 @@
 public class CountDigit {
 
     public static void main(String[] args) {
-        
+ //////////////////////////-Brute Force method-----------------------------------       
         int num = 657283;
-        int count = 0;
-        while(num != 0){
-            num = num / 10;
-            count++;
-        }
-        System.out.println(count);
+        // int count = 0;
+        // while(num != 0){
+        //     num = num / 10;
+        //     count++;
+        // }
+        // System.out.println(count);
+// -----------------------------------------------------------------------
+
+//-------------------Optimal Method--------------------------------
+
+        int countOfNum = (int) (Math.log10(num) + 1);
+        System.out.println(countOfNum);
+
 
     }
 
